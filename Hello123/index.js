ef8595fb76d44b8fd1ba9f116021b1fd8beca2ae
marlontamo <marlontamo@gmail.com>
2020-07-14 16:08:03 +0800

    added mysql package to connect to mysql database

--- Hello123/index.js
+++ Hello123/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'demoproject'
+});
+ 
+connection.connect();
 
 app.use(express.json());
 
@@ -10,11 +20,25 @@ const customers = [
 {id:4, name:'customer4'},
 {id:5, name:'customer5'},
 ];
+ //connect to mysql
+ app.get('/orders', (req,res) =>{
+  connection.query('SELECT * FROM orders', function (error, results, fields) {
+    if (error) throw error;
+    res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(results, null, 4));
  
+  });
+ });
+
 // listing all customers
 app.get('/api/customers', function (req, res) {
     res.send(customers);
   });
+  // app.get('/', function (req, res) {
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.sendfile('index.html', {root:__dirname});
+   
+  // }
 //inserting data
 app.post('/api/customer', function(req, res){
     const customer = {
